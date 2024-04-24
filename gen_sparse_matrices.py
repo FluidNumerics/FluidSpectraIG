6a1ea7aade84eb7d41105adb18762167cc2202c3
Joe Schoonover <joe@fluidnumerics.com>
2024-04-24 14:42:58 +0000

    Add tool to generate dirichlet matrices and slepc code to compute eigenvalues

--- gen_sparse_matrices.py
+++ gen_sparse_matrices.py
@@ -6,6 +6,9 @@ from fluidspectraig.nma import NMA
 import numpy as np
 import numpy.ma as ma
 
+from petsc4py import PETSc
+
+
 class splig:
     """ Sparse Laplacian - Irregular Geometry """
     def __init__(self, mask, action=None):
@@ -30,14 +33,21 @@ class splig:
 
       self.matrix_action = action
 
-
+filename_base = "squaredomain"
 # Create the model
 param = {'nx': 16,
          'ny': 16,
-         'Lx': 5120.0e3,
-         'Ly': 5120.0e3,
+         'Lx': 16.0,
+         'Ly': 16.0,
          'device': 'cuda',
          'dtype': torch.float64}
+
+# param = {'nx': 16,
+#          'ny': 16,
+#          'Lx': 5120.0e3,
+#          'Ly': 5120.0e3,
+#          'device': 'cuda',
+#          'dtype': torch.float64}
 
 nx = param['nx']
 ny = param['ny']
@@ -72,7 +82,6 @@ plt.close()
 
 # Generate impulse fields
 impulse = torch.zeros(sum(((4,4), model.xg.shape), ()),**model.arr_kwargs)
-print(impulse.shape)
 
 indices = np.indices(model.xg.shape)
 for j in np.arange(4):
@@ -100,4 +109,101 @@ for j in np.arange(4):
         plt.colorbar(fraction=0.046,location='right')
         plt.savefig(f'irf-{i}-{j}.png')
         plt.close()
+
+
+# For each matrix row, we need to compute the number of non-zero entries
+# Each row has at least one non-zero value
+nnz = np.ones(laplacian.ndof, dtype=np.int32)
+
+for r in np.arange(laplacian.ndof):
+    # Get i,j for this row
+    i = laplacian.i_indices[r]
+    j = laplacian.j_indices[r]
+
+    # Check south neighbor
+    k = laplacian.matrix_row[i,j-1]
+    if k >= 0:
+        nnz[r] += 1
+
+    # Check north neighbor
+    k = laplacian.matrix_row[i,j+1]
+    if k >= 0:
+        nnz[r] += 1
+
+    # Check west neighbor
+    k = laplacian.matrix_row[i-1,j]
+    if k >= 0:
+        nnz[r] += 1
+
+    # Check east neighbor
+    k = laplacian.matrix_row[i+1,j]
+    if k >= 0:
+        nnz[r] += 1
+
+print(f' Max(nnz) : {max(nnz)}')
+print(f' Min(nnz) : {min(nnz)}')
+
+# Now that we have the impulse and impulse response fields, we can look at creating the sparse matrix
+# Followed https://tbetcke.github.io/hpc_lecture_notes/petsc_for_sparse_systems.html as a guide
+A = PETSc.Mat()
+
+# https://petsc.org/release/manualpages/Mat/MatCreateSeqAIJ/
+# nnz = array containing the number of nonzeros in the various rows (possibly different for each row) or NULL
+A.createAIJ([laplacian.ndof, laplacian.ndof], nnz=nnz)
+
+# Now we can set the values
+irf_cpu = irf.squeeze().cpu().numpy()
+
+interior_template = mask == 1
+for j_shift in np.arange(4):
+    j_index = indices[1,:,:]-j_shift
+    for i_shift in np.arange(4):
+        i_index = indices[0,:,:]-i_shift
+
+        # Get the indices for the impulses
+        # This gives us a 2-d grid function that is true at
+        # each impulse location in the interior of the domain.
+        imp = ( ( (i_index % 4) + (j_index % 4) == 0 )*interior_template )
+        impulse_indices = [(i,j) for i, row in enumerate(imp) for j, entry in enumerate(row) if entry]
+
+        for i,j in impulse_indices:
+            # For each impulse, we get the dof index for the laplacian stencil points
+            # and fill the matrix
+
+            # Get the central point of the stencil (diagonal)
+            row = laplacian.matrix_row[i,j]
+            A.setValue(row,row,irf_cpu[i_shift,j_shift,i,j])
+
+            # Check south neighbor
+            col = laplacian.matrix_row[i,j-1]
+            if col >= 0:
+                A.setValue(row,col,irf_cpu[i_shift,j_shift,i,j-1])
+
+            # Check north neighbor
+            col = laplacian.matrix_row[i,j+1]
+            if col >= 0:
+                A.setValue(row,col,irf_cpu[i_shift,j_shift,i,j+1])
+
+            # Check west neighbor
+            col = laplacian.matrix_row[i-1,j]
+            if col >= 0:
+                A.setValue(row,col,irf_cpu[i_shift,j_shift,i-1,j])
+
+            # Check east neighbor
+            col = laplacian.matrix_row[i+1,j]
+            if col >= 0:
+                A.setValue(row,col,irf_cpu[i_shift,j_shift,i+1,j])
+
+# Assemble the matrix
+A.assemble()       
+print(f"Matrix Size : {A.size}")
+print(f"Matrix is symmetric : {A.isSymmetric()}")
+print(A.getInfo())
+
+# Write matrix to file
+filename = f"{filename_base}-{nx}-{ny}_dirichlet.dat"
+viewer = PETSc.Viewer().createBinary(filename, 'w')
+viewer(A)
+
+
 
